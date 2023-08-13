9baac2308c628b620359472a958e14b180574ba9
EdJeeOnGitHub <edjee96@gmail.com>
2023-08-12 21:39:19 -0500

    accepts control as an arg

--- scratch/rf-robustness.R
+++ scratch/rf-robustness.R
@@ -1,6 +1,6 @@
 script_options = docopt::docopt(
     stringr::str_glue("Usage:
-    rf-robustness.R  [options]
+    rf-robustness.R  [options] <controls>...
 
     Options:
         --load-fit
@@ -14,7 +14,11 @@ script_options = docopt::docopt(
     --save-fit
         --fit-path=data/stan_analysis_data
         --fit-file=REDUCED_FORM_ROBUSTNESS.rds
-        --output-path=temp-data
+        --output-path=temp-data/no-dist-cts
+        
+            mean_primary \
+            mean_floor \
+            mean_all_can_get_worms 
     " else commandArgs(trailingOnly = TRUE)
     # args = if (interactive()) "takeup cv --models=REDUCED_FORM_NO_RESTRICT --cmdstanr --include-paths=stan_models --update --output-path=data/stan_analysis_data --outputname=test --folds=2 --sequential" else commandArgs(trailingOnly = TRUE)
 ) 
@@ -27,6 +31,8 @@ library(marginaleffects)
 library(ggthemes)
 
 options(mc.cores = 4)
+
+dir.create(file.path(script_options$output_path))
 
 canva_palette_vibrant <- "Primary colors with a vibrant twist"
 
@@ -110,22 +116,32 @@ analysis_data = analysis_data %>%
         std_cluster_dist_to_pot = mean(standard_cluster.dist.to.pot, na.rm = TRUE)
     )
 
+controls = paste0(script_options$controls, collapse = "+")
+fml = 
+    paste0(
+        "dewormed ~ 0 + (assigned_treatment*assigned_dist_group) + (1 | county + cluster.id)  +",
+    controls
+)
+print(str_glue("Running Model: {fml}"))
+fml = as.formula(fml)
 
 default_priors = get_prior(
     data = analysis_data,
-    dewormed ~ (assigned_treatment*assigned_dist_group | county + cluster.id) + 
-        mean_primary + mean_floor + mean_all_can_get_worms + std_cluster_dist_to_pot, 
+    dewormed ~ (assigned_treatment*assigned_dist_group) +  (1 | county + cluster.id) + 
+        mean_primary + mean_floor + mean_all_can_get_worms, 
     family = bernoulli(link = "probit")
 )
 
 
-manual_priors = default_priors
+# manual_priors = default_priors
 
-# manual_priors$prior[9] = "normal(0, 0.1)"
-# manual_priors$prior[10] = "normal(0, 1)"
-# manual_priors$prior[18] = "normal(0, 0.1)"
-# manual_priors$prior[19] = "normal(0, 1)"
-manual_priors
+manual_priors = 
+    prior(normal(0, 1), class = b) +
+    prior(normal(0, 1), class = Intercept) +
+    prior(normal(0, 0.1), class = sd, group = cluster.id, lb = 0) +
+    prior(normal(0, 0.1), class = sd, group = county, lb = 0)
+
+
 
 if (script_options$load_fit) {
     rf_fit = read_rds(
@@ -138,8 +154,8 @@ if (script_options$load_fit) {
 
     rf_fit = brm(
         data = analysis_data,
-        dewormed ~ (assigned_treatment*assigned_dist_group | county + cluster.id) + 
-            mean_primary + mean_floor + mean_all_can_get_worms + std_cluster_dist_to_pot, 
+        dewormed ~ (assigned_treatment*assigned_dist_group) + (1 | county + cluster.id) + 
+            mean_primary + mean_floor + mean_all_can_get_worms, 
         family = bernoulli(link = "probit"),
         prior = manual_priors,
         backend = "cmdstanr",
@@ -187,7 +203,7 @@ pred_hat_dfs = map_dfr(
             mean_primary, 
             mean_floor, 
             mean_all_can_get_worms,
-            cluster_dist_to_pot
+            std_cluster_dist_to_pot
             ) %>%
         mutate(assigned_treatment = .x) %>%
         add_epred_rvars(rf_fit) %>%
@@ -244,20 +260,92 @@ level_df %>%
     )
 
 ggsave(
-    "temp-plots/rf-robust-levels.pdf",
+    file.path(
+        script_options$output_path,
+        "rf-robust-levels.pdf"
+    ),
     width = 8,
     height = 6
 )    
 
 
-te_plot = level_df %>%
+te_draw_df = level_df %>%
     mutate(
         te = level - level[assigned_treatment == "control"]
     ) %>%
-    filter(assigned_treatment != "control") %>%
+    filter(assigned_treatment != "control")
+
+bra_m_cal_draw_df = te_draw_df %>%
+    select(-level) %>%
+    pivot_wider(
+        names_from = assigned_treatment,
+        values_from = te
+    ) %>%
+    mutate(te = bracelet - calendar) %>%
+    select(assigned_dist_group, te) %>%
+    mutate(assigned_treatment = "bra - cal")
+
+te_draw_df = bind_rows(
+    te_draw_df,
+    bra_m_cal_draw_df
+)
+te_df = te_draw_df %>%
     median_qi(te, .width = 0.9) %>%
     to_broom_names() %>%
-    mutate(assigned_treatment = factor(assigned_treatment, levels = treatments)) %>%
+    mutate(assigned_treatment = factor(assigned_treatment, levels = treatments))
+
+
+# far - close
+far_m_close_draw_df = te_draw_df %>%
+    select(-level)  %>%
+    pivot_wider(
+        names_from = assigned_dist_group,
+        values_from = te
+    ) %>%
+    mutate(
+        far_minus_close = far - close
+    ) %>%
+    select(assigned_treatment, far_minus_close)
+# bra minus cal
+
+create_cis = function(.data) {
+  med_fun = function(x) {
+    mean_x = mean(x) %>% round(3)
+    conf.low = quantile(x, 0.05) %>% round(3)
+    conf.high = quantile(x, 0.95) %>% round(3)
+    return(paste0(
+      mean_x, " (", conf.low, ", ", conf.high, ")"
+    ))
+  }
+  .data %>%
+    mutate(across(where(is_rvar), med_fun))
+}
+
+te_draw_df %>%
+    select(assigned_dist_group, assigned_treatment, te) %>%
+    pivot_wider(
+        names_from = assigned_dist_group,
+        values_from = te
+    ) %>%
+    left_join(
+        far_m_close_draw_df 
+    )  %>%
+    create_cis()
+
+saveRDS(
+    te_draw_df,
+    file.path(
+        script_options$output_path,
+        "te-draw-df.rds"
+    )
+)
+
+
+
+
+
+
+te_plot = te_df %>%
     ggplot(aes(
         x = te,
         xmin = conf.low,
@@ -281,7 +369,10 @@ te_plot
 
 ggsave(
     plot = te_plot,
-    "temp-plots/rf-robust-te.pdf",
+    file.path(
+        script_options$output_path,
+        "rf-robust-te.pdf"
+    ),
     width = 8,
     height = 6
 )    
