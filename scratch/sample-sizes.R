35d5df0c53961da7e763729c3ee22573091761b1
Ed Jee <edjee96@gmail.com>
2025-04-22 17:24:30 -0500

    baseline sample

--- scratch/sample-sizes.R
+++ scratch/sample-sizes.R
@@ -1,10 +1,16 @@
 library(tidyverse)
+library(here)
+
+baseline.data = read_rds("temp-data/reclean_baseline_data.rds") # Not sampling data!
+
+
 
 
 baseline_data = read_rds(file.path("data", "takeup_baseline_data.rds"))
 
 all_endline_data = read_rds(file.path("data", "all_endline.rds"))
 
+baseline_data
 
 
 
@@ -88,3 +94,91 @@ census_data %>%
 
     census_data %>%
       summarise(mean(have_phone == "Yes", na.rm = TRUE))
+
+stop()
+rct.counties <- c("Busia", "Siaya", "Kakamega")
+busia.subcounties <- c("butula", "nambale", "teso south", "teso north") 
+siaya.subcounties <- c("gem", "ugenya", "ugunja")
+
+ke.lvl2.adm.data <- read_rds(here("data", "adm", "KEN_adm2.rds"))
+#ke.lvl3.adm.data <- read_rds("~/Data/TakeUp/KEN_adm3.rds")
+
+counties.adm.data <- ke.lvl2.adm.data[ke.lvl2.adm.data$NAME_1 %in% rct.counties, ] #, "Vihiga"), ]
+subcounties.adm.data <- counties.adm.data[!counties.adm.data$NAME_1 %in% c("Busia", "Siaya") | counties.adm.data$NAME_2 %in% str_to_title(c(busia.subcounties, siaya.subcounties)), ]
+
+datetime.format <- "%b %d, %Y %I:%M:%S %p"
+takeup.datetime.type <- col_datetime(datetime.format)
+takeup.date.type <- col_date(datetime.format)
+raw.data.path <- . %>% here("data", "raw-data", .)
+#### Baseline data ####
+validate.coords <- . %>% 
+  mutate(invalid.coord = 
+           (!is.na(lon) & (lon > county.bbox["x", "max"] | lon < county.bbox["x", "min"])) |
+           (!is.na(lat) & (lat > county.bbox["y", "max"] | lat < county.bbox["y", "min"])))
+tu.data.reader <- function(file.name, submit.datetime.type = NULL, .other.types = NULL) { # =  "%b %d, %Y %I:%M:%S %p") {
+  col.types <- list(SubmissionDate = if (is.null(submit.datetime.type)) takeup.datetime.type else submit.datetime.type,
+                                       manual_long = col_number(),
+                                       manual_lat = col_number()) %>% 
+    c(.other.types)
+  
+  read_csv(file.name, col_types = col.types) %>% 
+    mutate(isValidated = isValidated == "true") %>% 
+    rename(lat = `gps-Latitude`,
+           lon = `gps-Longitude`,
+           cluster.id = cluster_id) %>% 
+    filter(deviceid != "(web)") %>% 
+    mutate(lon = ifelse(is.na(lon), manual_long, lon),
+           lat = ifelse(is.na(lat), manual_lat, lat)) %>% 
+    validate.coords()
+}
+county.bbox <- counties.adm.data@bbox
+subcounty.bbox <- subcounties.adm.data@bbox
+census.data = read_rds("data/takeup_census.rds")
+
+raw_baseline_data <- tu.data.reader(raw.data.path("Baseline Survey.csv")) 
+
+stop()
+
+raw_baseline_data = raw_baseline_data %>%
+  mutate(
+    sub_date_filter = SubmissionDate >= "2016-09-05",
+    present_filter = present == 1 | !is.na(age),
+    consent_filter = !is.na(consent) & consent == 1
+  ) 
+  %>%
+  summarise(
+    across(contains('filter'), mean, na.rm = TRUE)
+  )
+
+
+raw_baseline_data %>%
+  count(sub_date_filter)
+# GOAL 2160
+# HAVE 2056
+
+raw_baseline_data %>%
+  filter(sub_date_filter == TRUE) %>%
+  count(present_filter) 
+
+raw_baseline_data %>%
+  filter(sub_date_filter == TRUE) %>%
+  filter(present_filter == TRUE) %>%
+  count(consent_filter)
+
+  filter(
+    sub_date_filter
+  )
+
+  filter(SubmissionDate >= "2016-09-05",
+         present == 1 | !is.na(age), 
+         !is.na(consent) & consent == 1) 
+
+reclean_baseline_data = tu.data.reader(raw.data.path("Baseline Survey.csv")) %>% 
+  filter(SubmissionDate >= "2016-09-05", 
+         present == 1 | !is.na(age), 
+         !is.na(consent) & consent == 1) %>% 
+  select(-county)  %>%
+  left_join(filter(., !invalid.coord) %>% identify.closest.cluster, "KEY") %>%
+  left_join(cluster.wave.county.data, "cluster.id") 
+
+
