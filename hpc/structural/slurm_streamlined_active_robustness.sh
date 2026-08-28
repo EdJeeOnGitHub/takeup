89b44b2adffd550751d83efdee543fab265beb24
EdJee <edjee96@gmail.com>
2026-08-28 05:47:33 -0500

    Respect Midway2 robustness walltime limit

--- hpc/structural/slurm_streamlined_active_robustness.sh
+++ hpc/structural/slurm_streamlined_active_robustness.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=20G
-#SBATCH --time=2-00:00:00
+#SBATCH --time=1-12:00:00
 #SBATCH --array=1-16
 #SBATCH --output=temp/log/slim-active-robust-%A-%a.log
 #SBATCH --error=temp/log/slim-active-robust-%A-%a.log
